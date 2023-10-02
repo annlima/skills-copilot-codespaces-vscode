b236048ac5e60625957a7078930ce3fa7d6dba5c
Andrea Lima <89811870+annlima@users.noreply.github.com>
2023-10-02 00:07:19 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,13 @@
-// Create web sever in comments.js
+// Create web sever for comment
+//====================================
+//Import module
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+const auth = require('../middlewares/auth');
+
+//Handle request
+router.get('/', commentController.getComment);
+router.post('/', auth, commentController.postComment);
+router.put('/:id', auth, commentController.putComment);
+router.delete('/:id', auth, commentController.deleteComment);
